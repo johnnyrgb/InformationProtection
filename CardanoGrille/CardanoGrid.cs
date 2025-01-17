3e5bc7d30fa27fc0c2f9b9708fd2e2e4416bdd81
Ivan <Ivan@FEIJOA-LAPTOP>
2025-01-17 16:39:51 +0300

    Add project files.

---
+++ CardanoGrille/CardanoGrid.cs
@@ -0,0 +1,19 @@
+﻿namespace CardanoGrid;
+
+public class CardanoGrid
+{
+    private int Width { get; set; }
+    private int Height { get; set; }
+
+    private int[] Holes { get; set; }
+    public CardanoGrid(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public bool Encrypt(string text)
+    {
+        return true;
+    }
+}
