563b8203376d5e9d0d81aa5433b53aff56effb25
Ivan Lopatin <vaneklopatin@outlook.com>
2025-03-06 14:02:27 +0300

    Streebog

---
+++ Streebog/Program.cs
@@ -0,0 +1,13 @@
+﻿using Streebog;
+
+var streebog = new Streebog.Streebog(512);
+
+var input = "Hello, world!";
+var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+var result = streebog.Run(inputBytes);
+
+// Convert the result to a base64 string
+var resultBase64 = Convert.ToBase64String(result);
+var resultString = BitConverter.ToString(result).Replace("-", "").ToLower();
+Console.WriteLine(resultString);
+Console.WriteLine(resultBase64);
